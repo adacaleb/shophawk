9778c5f8e369919e16c3e725c3095b8a77fdbc56
adacaleb <hatajc@gmail.com>
2023-05-09 14:05:29 -0500

    removed test code from slideshow_controller

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -21,57 +21,6 @@ class SlideshowsController < ApplicationController
     @totalSlides = 5
     @nextbtn = 2
     @currentSlide = 1
-
-    
-    dotJobs = []
-    CSV.foreach("app/assets/csv/yearlyUserValues.csv", 'r:iso-8859-1:utf-8', :quote_char => "|", headers: true, :col_sep => "`") do |row|
-      if row[0] != nil
-        dot = row[1].to_s.upcase
-        ops = Runlist.where(User_Value: row[0])
-        #puts ops
-        ops.each do |op|
-          dotJobs << op.Job
-          break
-        end
-        ops.each do |op|
-          case dot
-            when nil
-              op.dots = nil 
-            when "O"
-              op.dots = 1
-            when "0"
-              op.dots = 1
-            when "1"
-              op.dots = 1
-            when "OO"
-              op.dots = 2
-            when "00"
-              op.dots = 2
-            when "2"
-              op.dots = 2
-            when "OOO"
-              op.dots = 3
-            when "000"
-              op.dots = 3
-            when "3"
-              op.dots = 3
-          end
-          op.save
-        end
-      end
-    end
-    @operations = Runlist.where(status: "O").where().not(dots: nil) #select operations with dots and open
-    checkJobs = []
-    @operations.each do |op|
-      if dotJobs.include? op.Job
-        #puts "yes"
-      else
-        #puts "no"
-        op.dots = nil #removes the unneeded dots from the job
-        op.save
-      end
-    end
-
   end
   
 
